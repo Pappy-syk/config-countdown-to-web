7a3a2762934f274d7ebef3659c063e8a91c8fe3c
Pappy-syk <patiphan.seeyangnok@gmail.com>
2025-03-26 14:23:17 +0700

    fred
    อัพโค้ดครั้งเเรก

---
+++ popup/index.php
@@ -0,0 +1,4 @@
+<!-- เพิ่มใน <body> -->
+<iframe src="https://popup-e2kf.onrender.com/" width="100%" height="100%" style="border: none; position: fixed; top: 0; left: 0; z-index: 9999;">
+</iframe>
+<!--ตัวอย่างอยู่ README.md-->
---
+++ youtube-content/index.php
@@ -0,0 +1,5 @@
+<iframe class="video-iframe" width="768" height="436" src="https://no-popup.onrender.com/" title="YouTube video player" frameborder="0" 
+referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style="border-radius: 16px;">
+</iframe>
+<!--ใช้ถึงวันที่ 11/05/2025-->
+<!--เพิ่มใน class="video-content" ตัวอย่างอยู่ README.md-->
